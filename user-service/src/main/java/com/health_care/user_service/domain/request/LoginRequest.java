592e778baf0d4a768e419e449c9599465076a7fb
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-11-28 18:00:41 +0600

    auth config jwt with login api

---
+++ user-service/src/main/java/com/health_care/user_service/domain/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.health_care.user_service.domain.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String username;
+    private String password;
+}
