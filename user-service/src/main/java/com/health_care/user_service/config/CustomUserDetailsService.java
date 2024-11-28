d8dc65a89c0a84909005b2bba113ae421ac8b620
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-11-28 18:00:18 +0600

    auth config jwt

---
+++ user-service/src/main/java/com/health_care/user_service/config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.health_care.user_service.config;
+
+import com.health_care.user_service.domain.entity.User;
+import com.health_care.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> credential = repository.findByUsername(username);
+        return credential.map(CustomUserDetails::new).orElseThrow(() -> new UsernameNotFoundException("user not found with name :" + username));
+    }
+}
