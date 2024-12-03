e8c68f8e1c32e29da73cb72a1068eb4e73a26a3d
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-12-03 17:35:20 +0600

    create response & request class for doctor & minor change in api response

--- user-service/src/main/java/com/health_care/user_service/domain/common/ApiResponse.java
+++ user-service/src/main/java/com/health_care/user_service/domain/common/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.health_care.user_service.domain.common;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ApiResponse<T> implements Serializable {
     private String responseCode;
     private String responseMessage;
