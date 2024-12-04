4859eaf97f6c5abecd6b410e11e67df40cdbea5e
sahal-bs23 <sahal@brainstation-23.com>
2024-12-04 17:58:02 +0600

    frontend login integration

--- user-service/src/main/java/com/health_care/user_service/controller/LoginResource.java
+++ user-service/src/main/java/com/health_care/user_service/controller/LoginResource.java
@@ -1,26 +1,19 @@
 package com.health_care.user_service.controller;
 
 import com.health_care.user_service.common.utils.AppUtils;
-import com.health_care.user_service.common.utils.ResponseUtils;
 import com.health_care.user_service.domain.common.ApiResponse;
-import com.health_care.user_service.domain.entity.User;
 import com.health_care.user_service.domain.enums.ApiResponseCode;
 import com.health_care.user_service.domain.request.LoginRequest;
 import com.health_care.user_service.domain.response.TokenResponse;
-import com.health_care.user_service.repository.UserRepository;
 import com.health_care.user_service.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(AppUtils.BASE_URL)
