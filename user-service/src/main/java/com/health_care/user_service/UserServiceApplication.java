e8ff53c13b57b8eaa738fb0b380976a64641c381
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-12-10 14:57:08 +0600

    implement unique id generator logics & use it in user service as a jar

--- user-service/src/main/java/com/health_care/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/health_care/user_service/UserServiceApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.health_care.user_service", // Main package
+		"com.health_care.unique_id_generator" // Package for unique ID generator or other components
+})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
