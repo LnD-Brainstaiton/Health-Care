0b463f28b15f72c45df58c005143f0fcc165bb2c
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-12-11 15:05:50 +0600

    set admin is active true when save in bd

--- user-service/src/main/java/com/health_care/user_service/service/Impl/RegistrationServiceImpl.java
+++ user-service/src/main/java/com/health_care/user_service/service/Impl/RegistrationServiceImpl.java
@@ -224,6 +224,7 @@ public class RegistrationServiceImpl implements IRegistrationService {
                 .adminId(request.getUniqueId())
                 .lastname(request.getLastName())
                 .email(request.getEmail())
+                .isActive(Boolean.TRUE)
                 .build();
         adminRepository.save(admin);
     }
